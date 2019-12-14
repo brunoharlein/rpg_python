25bf95ae3cce6fc042b489e746ae484e8f4b667d
brunoharlein <bruno@harlein.com>
2019-12-14 11:45:39 +0100

    archer class ok

---
+++ characters/archer.py
@@ -0,0 +1,6 @@
+from .character import Character
+
+class Archer(Character):
+    """Class representing an archer character"""
+    def __init__(self, name = False):
+        super().__init__(350, 80, 30, 50, name)
