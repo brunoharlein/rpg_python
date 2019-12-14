7ca8f106bb6d2eb24321af018160a26b103f31dd
brunoharlein <bruno@harlein.com>
2019-12-14 11:54:32 +0100

    wizzard method heal ok

--- characters/wizzard.py
+++ characters/wizzard.py
@@ -11,3 +11,16 @@ class Wizzard(Character):
         # Represents the magical power
         self.mana = 200
 
+    def heal(self):
+        """Function to increase the life of the wizzard with the mana power"""
+        # The spell requires at least 50 points of mana
+        if self.mana >= 50:
+            print("{} lance un puissant sort de soin".format(self.name))
+            self.mana -= 50
+            self.life += 20
+            print("{} regagne 20 points de vie, santé actuelle : {}".format(self.name, self.life))
+        else :
+            print("{} tente de puiser en lui les forces nécessaires pour se soigner mais son mana n'est pas suffisant".format(self.name))
+
+    def __str__(self):
+        return "{} : vie = {}, mana = {}".format(self.name, self.life, self.mana)
