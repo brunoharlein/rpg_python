f35e6c3d556bb0497cd0b04fa4dd2ba1ae0a620e
brunoharlein <bruno@harlein.com>
2019-12-16 16:50:32 +0100

    add storyAgent with time and os

---
+++ game/storyAgent.py
@@ -0,0 +1,15 @@
+import os
+import time
+
+class storyAgent():
+    """ Mother class to all the objects that make the story living like the narrator or the arena
+    attributs : none
+    methods : transition
+    """
+    def __init__(self):
+        pass
+
+    def transition(self, waiting_time):
+        """Function to make a break and clear the screen"""
+        time.sleep(waiting_time)
+        os.system('cls||clear')
