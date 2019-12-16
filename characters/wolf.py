ee06691986e955cfe635f92b700523241d328a12
brunoharlein <bruno@harlein.com>
2019-12-16 16:48:15 +0100

    add wolf class

---
+++ characters/wolf.py
@@ -0,0 +1,6 @@
+from .character import Character
+
+class Wolf(Character):
+    """Class representing a wolf character"""
+    def __init__(self, name = False):
+        super().__init__(300, 30, 15, 40, name)
